984b1b16e69b21b167d7ff0ba4bb2b51505e2b30
pkbarnwal <42154337+pkbarnwal@users.noreply.github.com>
2020-01-10 02:57:56 +0530

    Create header.php

---
+++ header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <title>Exam Cell Automation</title>
+     <link rel="stylesheet" href="Bootstrap/CSS/bootstrap.min.css">
+     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="fontawesome-free-5.10.2-web/css/all.css">
+    <link href="Bootstrap/CSS/style.css" rel="stylesheet" type="text/css"/>
+<script src="Bootstrap/js/jquery-3.3.1.slim.min.js"></script>
+<script src="Bootstrap/js/popper.min.js"></script>
+<script src="Bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+  <header>
+  <div class="header">
+      <div class="p-1" id="topHeader">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 text-right">
+                <a class="p-2" href="tel:+9144461830" style="color:rgb(12, 25, 34)"><i class="fas fa-phone-square"></i> +(91) 9144461830</a>
+                <a class="p-2" href="mailto:techysheky@gmail.com" style="color:rgb(12, 25, 34)"><i class="fas fa-envelope"></i>techysheky@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+       
