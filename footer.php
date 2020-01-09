b2c17ca389f9fe52229fc86632ea99b8fb93c546
pkbarnwal <42154337+pkbarnwal@users.noreply.github.com>
2020-01-10 03:01:40 +0530

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,32 @@
+<footer class="full-footer">
+  <div class="container top-footer p-md-3 p-1">
+      <div class="row">
+          <div class="col-md-3 pl-4">
+              <p>This project is implement by our team .
+                <a href=" ">Read more...</a>
+              </p>
+                <a  style="color:rgb(255, 247, 247)" class="p-1"href="https://www.facebook.com/Techy Sheky"><i class="fab fa-2x fa-facebook-square"></i></a>
+                <a  style="color:rgb(255, 247, 247)" class="p-1"href="#"><i class="fab fa-2x fa-google-plus-square"></i></a>
+                <a  style="color:rgb(255, 247, 247)" class="p-1"href="#"><i class="fab fa-2x fa-twitter-square"></i></a>
+                <a  style="color:rgb(255, 247, 247)" class="p-1" href="#"><i class="fab fa-2x fa-instagram"></i></a>
+          </div>
+          <div class="col-md-4">
+          </div>
+          <div class="col-md-4 pl-4">
+               <h3>Important Links</h3>
+               <a  style="color:rgb(255, 247, 247)" href="#">Privacy Policy</a></br>
+               <a  style="color:rgb(255, 247, 247)" href="#">Youtube channel</a></br>
+               <a  style="color:rgb(255, 247, 247)" href="techysheky.blogspot.com">Blog Articles</a></br>
+          </div>
+        </div>
+      </div>
+          <div class="container-fluid bottom-footer pt-2">
+              <div class="row">
+                <div class="col-12 text-center">
+                  <p>Copyright @ 2019 - All rights reserved</p>
+                </div>
+              </div>
+           </div>
+    </footer>
+  </body>
+</html>
