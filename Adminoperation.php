b0463ff7b2e12652041197de1a705dad0d86f521
pkbarnwal <42154337+pkbarnwal@users.noreply.github.com>
2020-01-10 03:06:37 +0530

    Create Adminoperation.php

---
+++ Adminoperation.php
@@ -0,0 +1,55 @@
+<div class="bottomHeader">
+        <div class="container-fluid">
+            <nav class="navbar navbar-dark  navbar-expand-md" style="background-color:#5DADE2;width:100%;height:100px;">
+                    <a class="navbar-brand" href=" ">
+                      <img  class="img-fluid" src="images/logo.png" width="250px" alt="Mylogo">
+                    </a>
+                    <button data-toggle="collapse" data-target="#navbarToggler" type="button" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarToggler">
+
+          <div class="main_menu">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link"href="index.html">Home</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="adminlogin.php">Admin</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="student.html">Student</a>
+               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About us</a>
+              </li>
+            </ul>
+          </div> 
+        </div>
+      </div>
+  </div>
+</div>
+</div>
+</header>
+
+<Section class="admin-title">
+  <div class="container-fluid">
+       <div class="row">
+          <div class="col pl-3 pt-4">
+              <h5 style="font-size:18px;"><a style="color:rgb(65, 17, 17);"href="admindash.php">Back To DashBoard</a></h5>
+              <h4 style="font-size:18px;" align="right"><a  style="float:right" href="Logout.php">Log Out</a></h4>
+              <h1 class="text-center" style="font-size:3vw;">ADMIN DASHBOARD </h1>
+              
+           </div>
+        </div>
+       <!-- <div class="row">
+          <div class="column">
+             <h2 class="text-center">E-Exam Cell Admin</h2>
+         <div>
+       </div> -->
+      
+
+   </div> 
+</section>
+          
