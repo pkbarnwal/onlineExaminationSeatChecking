36ecc9d3587ff5c485b559eb15f70cd27da02bd8
pkbarnwal <42154337+pkbarnwal@users.noreply.github.com>
2020-01-10 02:52:52 +0530

    Create titleheader.php

---
+++ titleheader.php
@@ -0,0 +1,55 @@
+<div class="bottomHeader">
+        <div class="container-fluid">
+            <nav class="navbar navbar-dark  navbar-expand-md" style="background-color:#5DADE2;width:100%;height:100px;">
+                    <a class="navbar-brand" href=" ">
+                      <img  class="img-fluid" src="images/logo.png" width="250px" alt="Mylogo">
+                    </a>
+                    <button data-toggle="collapse" data-target="#navbarToggler" type="button" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+                    <div class="collapse navbar-collapse" id="navbarToggler">
+
+          <div class="main_menu">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link"href="index.html">Home</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="student.html">Student</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact us</a>
+               </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html">About us</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="logout.php">Log Out</a>
+              </li>
+            </ul>
+          </div> 
+        </div>
+      </div>
+  </div>
+</div>
+</div>
+</header>
+
+<Section class="admin-title">
+  <div class="container-fluid">
+    <div class="row">
+      <h2 class=" col-12 text-center pt-5">E-Exam Cell Admin</h2>
+</section>
+          
+ <section class="record">
+   <div class="container-fluid">
+            <div class="row">
+               <ul>
+                  <li><a href="admin/addstudent.php">Add Record</a></li>
+                  <li><a href="admin/delete.php">Delete Record</a></li>
+                  <li><a href="admin/update.php">Update Record</a></li>
+                  <li><a href="search.php">Search Record</a></li>
+              </ul>
+          </div> 
+        </div>
+    </div>
+ </nav>
+</section>
