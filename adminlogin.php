1301428662cddca113a74a9e1634b8db253d47ac
pkbarnwal <42154337+pkbarnwal@users.noreply.github.com>
2020-01-10 03:07:34 +0530

    Create adminlogin.php

---
+++ adminlogin.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+     <title>Exam Cell Automation</title>
+     <link rel="stylesheet" href="Bootstrap/CSS/bootstrap.min.css">
+     <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+    <link rel="stylesheet" href="fontawesome-free-5.10.2-web/css/all.css">
+    <link href="Bootstrap/CSS/style.css" rel="stylesheet" type="text/css"/>
+<script src="Bootstrap/js/jquery-3.3.1.slim.min.js"></script>
+<script src="Bootstrap/js/popper.min.js"></script>
+<script src="Bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<header>
+  <div class="header">
+      <div class="p-1" id="topHeader">
+          <div class="container">
+            <div class="row">
+              <div class="col-12 text-right">
+                <a class="p-2 text-center" href="tel:+9144461830" style="color:rgb(12, 25, 34)"><i class="fas fa-phone-square"></i> +(91) 9144461830</a>
+                <a class="p-2 text-center" href="mailto:techysheky@gmail.com" style="color:rgb(12, 25, 34)"><i class="fas fa-envelope"></i>techysheky@gmail.com</a>
+              </div>
+            </div>
+          </div>
+        </div>
+       <div class="bottomHeader">
+        <div class="container-fluid">
+            <nav class="navbar navbar-dark bg-primary navbar-expand-md" style="background-color:#5DADE2;">
+                <a class="navbar-brand" href=" ">
+                    <img  class="img-fluid" src="images/logo.png" width="250px" height="220px" alt="Mylogo">
+                </a>
+                <button data-toggle="collapse" data-target="#navbarToggler" type="button" class="navbar-toggler"><span class="navbar-toggler-icon"></span></button>
+                  <div class="collapse navbar-collapse" id="navbarToggler">
+                <ul class="navbar-nav">
+                   <li class="nav-item">
+                      <a class="nav-link"href="index.html">Home</a>
+                   </li>
+                   <li class="nav-item active">
+                      <a class="nav-link" href="adminlogin.php">Admin</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="student.html">Student</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="contact.html">Contact us</a>
+                  </li>
+                  <li class="nav-item">
+                     <a class="nav-link" href="about.html">About us</a>
+                 </li>
+                </ul>
+              </div>
+            </nav>
+       </div> 
+     </div>
+   </div>
+  </header>
+<section class="top-hero-section pt-4 pb-5 pl-5">
+    <div class="container">
+         <div class="row">
+           <div class="col-md-7 text-center">
+             <h4>Innovative-FastMover</h4>
+                <h1>Best Idea For exam Automation</h1>
+                <h5>Just Try to Implement it</h5></br>
+                  <button class="btn mb-2 mr-2 text-center" style="color:#fff; background-color:#004d80" btn-lg">Get Started</button>
+                  <a href="https://nevonprojects.com/exam-cell-automation-system/" style="color:blue;">Read more...</a>
+                </div>
+              <div class="col-md-5">
+                <img class="img-fluid pr-5 mt-3 mb-9" src="images/t3.jpg" alt="mycaroselpic style="height:140px;">
+      
+              </div>
+            </div>
+      
+          </div>
+      
+       </section>
+       <section class="breadcrumbs-section">
+          <div class="container p-1 p-sm-2">
+            <div class="row">
+              <div class="col-12">
+                <h2>Admin</h2>
+                <ol class="breadcrumb">
+                 <li class="breadcrumb-item"><a href="index.html">Home</a> </li>
+                 <li class="breadcrumb-item active">Admin</li>
+                </ol>
+              </div>
+            </div>
+          </div>
+       </section>
+<section> 
+    <div class="container">
+          <div clas="row">
+             <div class="col-12">
+                  <h2 class="text-center pt-2" style="text-decoration:underline;">Admin Login</h2> 
+                 <hr>
+            </div>
+          </div>
+    </div>
+</section> 
+<section class="login-admin">
+   <div class="container-fluid pt=2">
+      <div class="row">
+       <div class="hidden-xs col-sm-1  col-md-2 col-lg-2"> </div>
+         <div class="col-xs-12 col-sm-10  col-md-8 col-lg-8" styel="align:center;">
+            <form action="validation.php" method="post" enctype="">
+              <div class="form-group row">
+                <label for="Username" class="col-sm-3 form-control-label">Username:</label>
+                  <div class="col-sm-4">
+                      <input type="text" class="form-control  " id="username" name="username" placeholder="username" required="required">
+                  </div>
+              </div>
+              <div class="form-group row">
+                 <label for="password" class="col-sm-3 form-control-label  mt-md-3">Password:</label>
+                    <div class="col-sm-4">
+                        <input type="password" class="form-control" id="pass" name="password" placeholder=" password" required="required">
+                   </div>
+              </div>
+                  <div class="form-group row p-3">
+                      <input class="btn btn-success" type="submit" value="LOGIN" name="Login"/>
+                 </div>
+                    </center>
+                  </form>
+                </div>
+                <div class="hidden-xs col-sm-1  col-md-2 col-lg-2"> </div>
+              </div>
+            </div>
+          </div>
+        </div>
+</section> 
+<footer class="full-footer">
+    <div class="container top-footer p-md-3 p-1">
+         <div class="row">
+             <div class="col-md-3 pl-4 text-center">
+                    <p>This project is implement by our team .<a href=" ">Read more...</a></p>
+                        <a  style="color:rgb(255, 247, 247)" class="p-1"href="https://www.facebook.com/abhishekpathak">    <i class="fab fa-2x fa-facebook-square"></i></a>
+                         <a style="color:rgb(255, 247, 247)" class="p-1"href="#"><i class="fab fa-2x fa-google-plus-square"></i></a>
+                         <a style="color:rgb(255, 247, 247)" class="p-1"href="#"><i class="fab fa-2x fa-twitter-square"></i></a>
+                         <a style="color:rgb(255, 247, 247)" class="p-1" href="abhishekpathak654.instagram.com"><i class="fab fa-2x fa-instagram"></i></a>
+              </div>
+                <div class="col-md-4 ">
+                </div>
+                    <div class="col-md-4 pl-4 text-center">
+                        <h3 style="color:rgb(255,247,247);">Important Links</h3>
+                        <a style="color:rgb(255,247,247);" href="#">Privacy Policy</a></br>
+                        <a style="color:rgb(255,247,247);" href="#">Youtube channel</a></br>
+                        <a style="color:rgb(255,247,247);" href="techysheky.blogspot.com">Blog Articles</a></br>
+                        
+                   </div>
+                 </div>
+               </div>
+               <div class="container-fluid bottom-footer pt-2">
+                   <div class="row">
+                     <div class="col-12 text-center">
+                       <p>Copyright @ 2019 - All rights reserved</p>
+             
+                     </div>
+                   </div>
+                </div>
+</footer>
+
+  </body>
+</html>
+
